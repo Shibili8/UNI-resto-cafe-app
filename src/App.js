ca099a0cc315502eb0092bceca54de13ed8d9088
Shibili8 <shibilishibizz99@gmail.comgit config --global user.email shibilishibizz99@gmail.comgit config --global user.email shibilishibizz99@gmail.com>
2025-08-18 15:48:59 +0000

    Updated restaurant app with latest changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import {Route, Switch, BrowserRouter} from "react-router-dom"
 import MenuCard from './components/MenuCard'
 import './App.css'
 
-const App = () => <MenuCard />
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={MenuCard}/>
+        </Switch>
+    </BrowserRouter>
+)
 
 export default App
